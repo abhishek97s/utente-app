9fecbdcc0b0b01e7166f24e6d7d94c5b9c3cbea4
Abhishek Sharma <abhisharmadholi@gmail.com>
2021-07-10 11:49:30 +0530

    my first commit

---
+++ src/main/java/com/deuexsolutions/utente/UtenteAppApplication.java
@@ -0,0 +1,13 @@
+package com.deuexsolutions.utente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtenteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtenteAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deuexsolutions/utente/UtenteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deuexsolutions.utente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtenteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
